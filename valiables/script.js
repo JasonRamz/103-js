2f8b4c525f13ba26bcf83361ad345b9f5dde8b41
JasonRamz <12ramirezjason@gmail.com>
2023-08-26 11:52:02 -0700

    Variables

---
+++ valiables/script.js
@@ -0,0 +1,50 @@
+console.log("Scripts");
+var message = "hello world";
+let num1;
+num1=10
+let num2=20;
+let sum =num1+num2;
+let firstName = " Jason"
+
+console.log(message,num1 + num2);
+
+document.write("The result of the sum is:" + " the calculation was made by" + firstName);
+
+message="Welcome to coding";
+
+console.log(message);
+
+//exercise #1
+let jobTitle="Body Guard";
+let geoLocation="San Diego";
+let partnerName= "donna";
+let numberChildren= 0;
+
+console.log("you will be a " + jobTitle+ " in" + geoLocation + " , and married to " + partnerName + " with" + numberChildren + "kids");
+
+document.write(
+    `
+    <h2> Exercise 1 </h2>
+    <p>
+        You will be a ${jobTitle} in ${geoLocation} , and married to ${partnerName} with ${numberChildren} kids.
+    </p>
+    `
+);
+//exercise #2
+let Name="Jason Ramirez";
+let Email="Jason@gmail.com";
+let salaryYearly= 300000;
+
+
+document.write(
+    `
+    <p>Name: ${Name}</p>
+    <p>Email:${Email}</p>
+    <p>Salary: ${salaryYearly}</p>
+    `
+);
+
+
+
+
+
